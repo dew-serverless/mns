d518f3ac6ab44ae7026896ec2a2f3688f93b65d3
Li Zhineng <im@zhineng.li>
2023-11-07 11:12:43 +0800

    configures client

--- tests/MnsClientTest.php
+++ tests/MnsClientTest.php
@@ -10,3 +10,11 @@ test('request has authorization header', function () {
     $mns->get('/');
     $mns->assertSent(fn ($request) => $request->hasHeader('Authorization') && $request->getHeaderLine('Authorization') === 'MNS key:foo');
 });
+
+test('configures request options', function () {
+    $mns = new MnsClient('https://123456789101112.mns.cn-hangzhou.aliyuncs.com', 'key', 'secret');
+    $mns->configure(['headers' => ['X-Foo' => 'Bar']]);
+    $mns->fake();
+    $mns->get('/');
+    $mns->assertSent(fn ($request) => $request->getHeaderLine('X-Foo') === 'Bar');
+});
